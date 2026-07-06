6345923a92927c05deb084d74fb266747d27f733
Luis Blanco <luisblanco1337@gmail.com>
2026-07-07 00:05:07 +0400

    Stop tracking generated build artifacts

--- dist/qml-overlay-material.d.ts
+++
@@ -1,3 +0,0 @@
-import type { TInitMaterialOpts, TNewableQmlOverlayMaterial } from './types.ts';
-declare const init: (opts: TInitMaterialOpts) => TNewableQmlOverlayMaterial | null;
-export default init;
--- dist/qml-overlay.d.ts
+++
@@ -1,3 +0,0 @@
-import type { TInitOverlayOpts, TNewableQmlOverlay } from './types.ts';
-declare const init: (opts: TInitOverlayOpts) => TNewableQmlOverlay | null;
-export default init;
